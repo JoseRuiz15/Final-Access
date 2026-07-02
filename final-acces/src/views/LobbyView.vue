0455c12e4c47999adb2fe6acb27c62ad7e1a050b
Leydy <amapolaverde53@gmail.com>
2026-07-01 19:58:07 -0500

    detalles

--- final-acces/src/views/LobbyView.vue
+++ final-acces/src/views/LobbyView.vue
@@ -2,7 +2,8 @@
 import { useRouter } from 'vue-router'
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
-
+import { ref } from 'vue'
+const mostrarConfirmacion = ref(false)
 const router = useRouter()
 
 function irGameMode() {
@@ -16,6 +17,11 @@ function irCharacters() {
 function irStore() {
   router.push('/store')
 }
+
+function irLoggin() {
+  router.push('/login')
+}
+
 onMounted(() => {
   initFlowbite()
 })
@@ -83,8 +89,10 @@ onMounted(() => {
             <img src="/img/mensajes.png" alt="mensajes" class="w-full h-full object-cover" />
           </button>
 
-          <button class="w-[50px] h-[50px] border-3 border-[#41334B] rounded-lg overflow-hidden">
-            <img src="/img/salida.png" alt="cierre de sesión" class="w-full h-full object-cover" />
+          <button @click="mostrarConfirmacion = true" class="w-[50px] h-[50px] border-3 border-[#41334B] rounded-lg overflow-hidden">
+            <img src="/img/salida.png"
+                alt="cierre de sesión"
+                class="w-full h-full object-cover"/>
           </button>
         </div>
       </header>
@@ -93,19 +101,15 @@ onMounted(() => {
       <main class="flex justify-between px-10 pt-20 flex-1">
         <!-- Lado izquierdo -->
         <section class="flex flex-col gap-10">
-          <div
-            class="h-20 w-80 border-4 border-[#00FFEA] bg-[#000004] rounded-lg flex items-center"
-          >
+          <button class="h-20 w-80 border-4 border-[#00FFEA] bg-[#000004] rounded-lg flex items-center transition-all duration-300 hover:shadow-[0_0_20px_#00FFEA] hover:-translate-y-2">
             <div class="h-15 w-15 ml-[10px] rounded-lg">
               <img src="/img/clasificatoria.png" alt="imagen" />
             </div>
 
             <h1 class="text-[#9898BD] font-pixel text-[15px] ml-[10px]">CLASIFICATORIA</h1>
-          </div>
+          </button>
 
-          <div
-            class="h-20 w-80 border-4 border-[#00FFEA] bg-[#000004] rounded-lg flex items-center"
-          >
+          <button class="h-20 w-80 border-4 border-[#00FFEA] bg-[#000004] rounded-lg flex items-center transition-all duration-300 hover:shadow-[0_0_20px_#00FFEA] hover:-translate-y-2">
             <div class="h-15 w-15 ml-[10px] rounded-lg">
               <img src="/img/amigos.png" alt="imagen" />
             </div>
@@ -115,7 +119,7 @@ onMounted(() => {
 
               <p class="text-[#777787] font-pixel text-[12px]">CONECTADOS: 0</p>
             </div>
-          </div>
+          </button>
         </section>
 
         <!-- Lado derecho -->
@@ -303,4 +307,27 @@ onMounted(() => {
       </main>
     </div>
   </section>
+
+  <div v-if="mostrarConfirmacion" class="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
+
+  <div class="w-[700px] h-[400px] bg-black border-4 border-[#8B7A91] rounded-lg">
+    <h1 class="font-pixel text-[#FB00FF] text-3xl text-center mt-8">
+      ¿ESTÁS SEGURO DE QUE QUIERES SALIR?
+    </h1>
+
+    <div class="flex flex-col items-center gap-8 mt-16">
+
+      <button @click="irLoggin" class="font-pixel text-white w-[250px] h-[60px] bg-[#2A1A4B] rounded-xl border-2 border-fuchsia-500 transition-all duration-300 hover:border-fuchsia-500 hover:shadow-[0_0_20px_#FB00FF] hover:-translate-y-2">
+        SI
+      </button>
+
+      <button
+        @click="mostrarConfirmacion = false"
+        class="font-pixel text-white w-[250px] h-[60px] bg-[#2A1A4B] rounded-xl border-2 border-fuchsia-500 transition-all duration-300 hover:border-fuchsia-500 hover:shadow-[0_0_20px_#FB00FF] hover:-translate-y-2"
+      >
+        NO
+      </button>
+    </div>
+  </div>
+</div>
 </template>
