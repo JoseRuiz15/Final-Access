cd0a0c211ec220a12b6d6d691fb410a8b55e095f
Slendy Moreno <slendymorenoduran@gmail.com>
2026-09-25 21:20:52 -0500

    gabriel que hiciste

---
+++ final-acces/src/game/configs/characters.js
@@ -0,0 +1,21 @@
+export const CHARACTERS = {
+    cosa1: {
+        id: 'cosa1',
+        name: 'cosa 1',
+        texture: 'player',
+        walkTexture: 'playerWalk',
+        attackType: 'melee',
+        stats: {
+            vida: 100,
+            velocidad: 160,
+            daño: 20,
+        },
+        //colocar animaciones aqui
+    },
+
+    cosa2: {
+        id:'cosa2',
+        name: 'cosa 2',
+        
+    }
+}
